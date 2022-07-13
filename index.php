b88476d2447987293f0d47f2382ccebd3f0a0500
tolik129 <boykoanatoliy@ukr.net>
2022-07-13 18:01:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package test
+ */
+
+get_header();
+?>
+
+<div class="container">
+	<a href="">
+		<img src="<?php bloginfo('template_url'); ?>/img/item-1.png" alt="">
+       <div class="wraper">
+       	  <div class="wraper__item ">
+       	  	<h3>Программа</h3>
+       	  	<p>Мастер-классы от звeзд украинского шоу-бизнеса</p>
+       	  </div>
+       	  <div class="wraper__item icon">
+       	  	<img src="<?php bloginfo('template_url'); ?>/img/icon.png" alt="">
+       	  </div>
+       </div>
+	</a>
+	<a href="">
+		<img src="<?php bloginfo('template_url'); ?>/img/item-1.png" alt="">
+       <div class="wraper">
+       	  <div class="wraper__item">
+       	  	<h3>Программа</h3>
+       	  	<p>Мастер-классы от звeзд украинского шоу-бизнеса</p>
+       	  </div>
+       	  <div class="wraper__item icon">
+       	  	<img src="<?php bloginfo('template_url'); ?>/img/icon.png" alt="">
+       	  </div>
+       </div>
+	</a>
+	<a href="">
+		<img src="<?php bloginfo('template_url'); ?>/img/item-1.png" alt="">
+       <div class="wraper">
+       	  <div class="wraper__item">
+       	  	<h3>Программа</h3>
+       	  	<p>Мастер-классы от звeзд украинского шоу-бизнеса</p>
+       	  </div>
+       	  <div class="wraper__item icon">
+       	  	<img src="<?php bloginfo('template_url'); ?>/img/icon.png" alt="">
+       	  </div>
+       </div>
+	</a>
+	<a href="">
+		<img src="<?php bloginfo('template_url'); ?>/img/item-1.png" alt="">
+       <div class="wraper">
+       	  <div class="wraper__item">
+       	  	<h3>Программа</h3>
+       	  	<p>Мастер-классы от звeзд украинского шоу-бизнеса</p>
+       	  </div>
+       	  <div class="wraper__item icon">
+       	  	<img src="<?php bloginfo('template_url'); ?>/img/icon.png" alt="">
+       	  </div>
+       </div>
+	</a>
+	<a href="">
+		<img src="<?php bloginfo('template_url'); ?>/img/item-1.png" alt="">
+       <div class="wraper">
+       	  <div class="wraper__item">
+       	  	<h3>Программа</h3>
+       	  	<p>Мастер-классы от звeзд украинского шоу-бизнеса</p>
+       	  </div>
+       	  <div class="wraper__item icon">
+       	  	<img src="<?php bloginfo('template_url'); ?>/img/icon.png" alt="">
+       	  </div>
+       </div>
+	</a>
+	<a href="">
+		<img src="<?php bloginfo('template_url'); ?>/img/item-1.png" alt="">
+       <div class="wraper">
+       	  <div class="wraper__item">
+       	  	<h3>Программа</h3>
+       	  	<p>Мастер-классы от звeзд украинского шоу-бизнеса</p>
+       	  </div>
+       	  <div class="wraper__item icon">
+       	  	<img src="<?php bloginfo('template_url'); ?>/img/icon.png" alt="">
+       	  </div>
+       </div>
+	</a>
+</div>	
+
+<?php
+
+get_footer();
